c905f1e235edf8c7973f5e74884faa0aab98d097
“Adefemi <myseamas@gmail.com>
2022-11-29 05:22:51 -0800

    Task

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -3,7 +3,13 @@
 
 #include <stdlib.h>
 
-
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
 typedef struct listint_s
 {
 int n;
@@ -12,7 +18,7 @@ struct listint_s *next;
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
-void free_listint(liistint_t *head);
+void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
