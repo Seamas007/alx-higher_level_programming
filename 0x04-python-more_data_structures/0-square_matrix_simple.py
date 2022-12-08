1babfe6165d264cacf7cdf2b345fc965de6123ca
“Adefemi <myseamas@gmail.com>
2022-12-08 03:27:37 -0800

    Task 0x04

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 # 0-square_matrix_simple.py
-# Brennan D Baraban <375@holbertonschool.com>
-
 
 def square_matrix_simple(matrix=[]):
     """Compute the square value of all integers of a matrix."""
