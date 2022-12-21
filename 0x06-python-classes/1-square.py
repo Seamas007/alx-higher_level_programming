081c0117891c69eeb6e9e4bc4acf01e7c898ea5d
“Adefemi <myseamas@gmail.com>
2022-12-21 04:23:06 -0800

    0x06

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,5 +5,6 @@ classs Square:
     """Represents a square"""
     def __init__(self, size):
         """Initializing this square class
-        Args: size - represents the size of the square"""
+        Args: size - represents the size of the square defined
+        """
         self.__size = size
