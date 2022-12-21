026fdc35cd94e66ee380f9f3cb00da52030b9510
“Adefemi <myseamas@gmail.com>
2022-12-21 04:13:10 -0800

    0x06

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-square.py by Adefemi Oreyomi
+"""Defines a square"""
+classs Square:
+    """Represents a square"""
+    def __init__(self, size):
+        """Initializing this square class
+        Args: size - represents the size of the square"""
+        self.__size = size
