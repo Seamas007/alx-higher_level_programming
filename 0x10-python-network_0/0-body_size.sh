27f1b420b164fb41b23597544c9b09980dbcab6c
Seamas007 <olufy007@gmail.com>
2023-03-30 04:14:05 -0700

    Task Network_0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-
-if [[ $# -eq 0 ]]; then
-    echo "Usage: $0 <url>"
-    exit 1
-fi
-
-url="$1"
-
-response=$(curl -sI $url)
-content_length=$(echo "$response" | grep -i content-length | awk '{print $2}')
-
-if [[ -z $content_length ]]; then
-    echo "Could not determine content length"
-    exit 1
-fi
-
-echo "Content length: $content_length bytes"
+# send a request to an URL with curl, and displays the size of the body of the response
+curl -s "$1" | wc -c
