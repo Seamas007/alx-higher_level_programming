dab4d3b85633a98cd79c18b51fe8ce414e924bce
Seamas007 <olufy007@gmail.com>
2023-03-31 01:22:49 -0700

    Task Network_0

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# send a POST request to the passed URL using curl, and display the body of the response
+# Bash scripts that sends a POST request to a given URL.
 curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
-
